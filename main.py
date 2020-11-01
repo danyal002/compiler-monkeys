3079f398f11598e5532bd3a8c784aa4cbd1e10f5
danyal002 <50880807+danyal002@users.noreply.github.com>
2020-11-01 13:05:09 -0500

    Tweaked the capture() function

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ import requests
 
 
 def capture():
-    if(os.path.exists('/deskop/images/snapshot.jpg')):
-        os.remove('/deskop/images/snapshot.jpg')
+    if(os.path.exists('/home/pi/Desktop/images/snapshot.jpg')):
+        os.remove('/home/pi/Desktop/images/snapshot.jpg')
     camera = picamera.PiCamera()
-    camera.capture("snapshot.jpg", resize=(500, 500))
+    camera.hflip = True
+    camera.capture("/home/pi/Desktop/images/snapshot.jpg", resize=(500, 500))
 
 
 def upload():
-    fileToSend = "/desktop/images/snapshot.jpg"
+    fileToSend = "/home/pi/Desktop/images/snapshot.jpg"
     url = "http://104.198.69.57:5000/upload"
     files = [('DoorImage', (fileToSend, open(
         fileToSend, 'rb'), 'application/octet'))]
@@ -22,10 +23,12 @@ def upload():
 
 
 # SPACE BELOW FOR HARWARE STUFF - DO NOT TYPE ABOVE THIS LINE LOLOLOL
-arduino = serial.Serial('COM5', 9600, timeout=1)
-time.sleep(10)
-sio = io.TextIOWrapper(io.BufferedRWPair(arduino, arduino))
-arduino.write(b'L')
-print("sentL")
-time.sleep(10)
-arduino.write(b'U')
+# arduino = serial.Serial('COM5', 9600, timeout=1)
+# time.sleep(10)
+# sio = io.TextIOWrapper(io.BufferedRWPair(arduino, arduino))
+# arduino.write(b'L')
+# print("sentL")
+# time.sleep(10)
+# arduino.write(b'U')
+
+capture()
