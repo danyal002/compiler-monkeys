f9b6fb2906adbc279ecebb7290e32975e9cda70e
danyal002 <danyal.ak002@gmail.com>
2020-11-01 10:27:12 -0500

    Code Refactoring & Cleanup

--- Servo_Control__Serial_/Servo_Control__Serial_.ino
+++ Servo_Control__Serial_/Servo_Control__Serial_.ino
@@ -1,37 +1,11 @@
-//#include <Servo.h>
-//
-//Servo servo1;  
-//long num;     
-//
-//void setup()
-//{
-// servo1.attach(9);
-//Serial.begin(9600); 
-////Serial.print("Enter Position = ");
-//}
-//
-//void loop() 
-//{ 
-//  if(Serial.available()>0)
-//  { 
-//  num= Serial.parseInt();   
-//  Serial.print(num);  
-//  Serial.println(" degrees");
-//  Serial.print("Enter Position = ");
-//  servo1.write(num);
-//  Serial.end();
-//  Serial.begin(9600);
-//  }
-//  
-//}
-
 #include <Servo.h>
-Servo myservo; 
+Servo myservo;
 String inByte;
 int pos;
 
-void setup() {
- 
+void setup()
+{
+
   myservo.attach(9);
   myservo.write(90);
   Serial.begin(9600);
@@ -39,21 +13,21 @@ void setup() {
 
 void loop()
 {
-  if(Serial.available())  // if data available in serial port
-    { 
-    inByte = Serial.readString(); // read data until newline
-    pos = inByte.toInt();   // change datatype from string to integer 
+  if (Serial.available())
+  {
+    inByte = Serial.readString();
+    pos = inByte.toInt();
     Serial.print(inByte);
-    if(strstr(const_cast<char*>(inByte.c_str()),"L") != NULL){
+    if (strstr(const_cast<char *>(inByte.c_str()), "L") != NULL)
+    {
       myservo.write(23);
       Serial.print(inByte);
       Serial.print("Locking\n");
-    }else if(strstr(const_cast<char*>(inByte.c_str()),"U") != NULL){
+    }
+    else if (strstr(const_cast<char *>(inByte.c_str()), "U") != NULL)
+    {
       myservo.write(90);
       Serial.print("Unlocking\n");
     }
-//    myservo.write(pos);     // move servo
-//    Serial.print("Servo in position: ");  
-//    Serial.println(inByte);
-    }
+  }
 }
--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-import serial                                           # import serial library
+import serial                                          
 import time
 import io
-
-
-
-
-
-
-
-
-
-
-
 
 # SPACE BELOW FOR HARWARE STUFF - DO NOT TYPE ABOVE THIS LINE LOLOLOL
 arduino = serial.Serial('COM5', 9600, timeout=1)
@@ -19,38 +8,6 @@ time.sleep(10)
 sio = io.TextIOWrapper(io.BufferedRWPair(arduino, arduino))
 arduino.write(b'L')
 print("sentL")
-# sio.write("L\n")
-# sio.flush()
 time.sleep(10)
 arduino.write(b'U')
-# sio.write("U\n")
-# sio.flush()
 
-# import serial
-# import io
-# ser = serial.serial_for_url('loop://', timeout=1)
-# sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
-
-# sio.write(unicode("hello\n"))
-# sio.flush() # it is buffering. required to get the data out *now*
-# hello = sio.readline()
-# print(hello == unicode("hello\n"))
-# reachedPos = str(arduino.readline())
-                                            # create loop
-
-	                          # write position to serial port
-   # reachedPos = str(arduino.readline())            # read serial port for arduino echo
-   # print(reachedPos)                               # print arduino echo to console
-
-
-
-# def lock():                    
-#     if(arduino.)
-#     global arduino
-#     #arduino.write(bytes("155\n", "utf-8"))   
-#     arduino.write(b'155')                                             
-#     #reachedPos = str(arduino.readline())  
-      
-
-# while True:
-#     lock()
