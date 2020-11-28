24f9314cc43091eb9218d10691fd292f35a79ece
danyal002 <danyal.ak002@gmail.com>
2020-11-28 13:13:48 -0500

    Create unlock.py

---
+++ unlock.py
@@ -0,0 +1,18 @@
+ 
+import serial
+import time
+import io
+import picamera
+import os
+import requests
+
+arduino = serial.Serial('/dev/ttyUSB0', 9600, timeout=1) # Gotta change the com port when we connect the arduin to the pi
+time.sleep(7)
+
+def lock():
+    arduino.write(b'L')
+
+def unlock():
+    arduino.write(b'U')
+
+unlock();
