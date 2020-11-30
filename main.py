8da3be2801faf5a939ccff417838af35492de9cc
danyal002 <danyal.ak002@gmail.com>
2020-11-30 16:28:56 -0500

    Final Changes

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
- 
 import serial
 import time
 import io
@@ -6,19 +5,22 @@ import picamera
 import os
 import requests
 
-arduino = serial.Serial('/dev/ttyUSB0', 9600, timeout=1) # Gotta change the com port when we connect the arduin to the pi
+# Gotta change the com port when we connect the arduin to the pi
+arduino = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 time.sleep(7)
 
+
 def capture():
-    if(os.path.exists('/home/pi/Desktop/images/snapshot.jpg')):
-        os.remove('/home/pi/Desktop/images/snapshot.jpg')
+    if(os.path.exists('/home/pi/Desktop/images/guest_photo.jpg')):
+        os.remove('/home/pi/Desktop/images/guest_photo.jpg')
     camera = picamera.PiCamera()
     camera.hflip = True
-    camera.capture("/home/pi/Desktop/images/snapshot.jpg", resize=(500, 500))
+    camera.capture("/home/pi/Desktop/images/guest_photo.jpg")
+    camera.close()
 
 
 def upload():
-    fileToSend = "/home/pi/Desktop/images/snapshot.jpg"
+    fileToSend = "/home/pi/Desktop/images/guest_photo.jpg"
     url = "http://104.198.69.57:8080/api/identifyguest"
     files = [('guest_photo', (fileToSend, open(
         fileToSend, 'rb'), 'application/octet'))]
@@ -26,21 +28,25 @@ def upload():
     result = str(r.content, "utf-8")
     return result
 
+
 def lock():
     arduino.write(b'L')
+
 
 def unlock():
     arduino.write(b'U')
 
+
+lock()
 while(True):
-    #time.sleep(2) #should be changed on how often we want to run the entire process.
+    # should be changed on how often we want to run the entire process.
+    time.sleep(3)
     capture()
     result = upload()
     print(result)
-    if(result == "True"):
+    if(result == "true"):
         unlock()
-        time.sleep(3)    #Time after which the door should automatically be locked.
+        # Time after which the door should automatically be locked.
+        time.sleep(5)
         lock()
-    break
-
-      
+    # break
